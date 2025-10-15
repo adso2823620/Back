3bee1a8b3f6ff57b7988f8d0f99bca94c43a6e06
unknown <cncideasapp@gmail.com>
2025-10-15 18:57:56 -0500

    Primer envio de proyecto

---
+++ src/main/java/com/reto/MujeresDigitales/MujeresDigitalesApplication.java
@@ -0,0 +1,13 @@
+package com.reto.MujeresDigitales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MujeresDigitalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MujeresDigitalesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reto/MujeresDigitales/MujeresDigitalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reto.MujeresDigitales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MujeresDigitalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
